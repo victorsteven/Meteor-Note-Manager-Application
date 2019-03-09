05273edbfedc828917389107107fce299380764b
victorsteven <chikodi543@gmail.com>
2019-03-09 12:16:43 +0100

    Add functionality to add note

--- client/main.js
+++ client/main.js
@@ -1,5 +1,40 @@
 import { Template } from 'meteor/templating';
+import { Notes } from '../lib/collections.js';
 
 import './main.html';
 
+Template.body.helpers({
 
+  // notes: [
+  //   {text: 'My Note 1'},
+  //   {text: 'My Note 2'},
+  //   {text: 'My Note 3'}
+  // ],
+
+  notes(){
+    return Notes.find({});
+  }
+});
+
+Template.add.events({
+  'submit .add-form': function(){
+    event.preventDefault();
+
+    const target = event.target;
+    const text = target.text.value;
+
+    // console.log(text)
+    //insert note:
+    Notes.insert({
+      text,
+      createdAt: new Date()
+    });
+
+    target.text.value = '';
+
+    $('#addmodal').modal('close');
+
+
+    return false;
+  }
+})
--- server/main.js
+++ server/main.js
@@ -1,5 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 
+export const Notes = new Mongo.Collection('notes');
+
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
